5d9e1818c1e0486ecc6993cfcd9213e0b6fff7b8
kartoonjoy <101595687+kartoonjoy@users.noreply.github.com>
2022-12-21 10:10:59 -0600

    Delete ccmp-contracts directory

--- ccmp-contracts/contracts/interfaces/ICCMPExecutor.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ICCMPExecutor {
-    function execute(address _to, bytes calldata _calldata)
-        external
-        returns (bool success, bytes memory returndata);
-}
--- ccmp-contracts/contracts/structures/Constants.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
-
-abstract contract Constants {
-    address internal constant NATIVE_ADDRESS =
-        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
-}
